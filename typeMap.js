3154b64504c07c9063ea686f9be9eb66e12cf39e
Dustin Machi <dmachi@dojotoolkit.org>
2016-03-25 21:36:01 -0400

    fix type map

--- typeMap.js
+++ typeMap.js
@@ -4,10 +4,8 @@ var map = {
 	"fq": "reads",
 	"fastq": "reads",
 	"tgz": "tar_gz",
-    	".fna": "contigs",
-	".fq": "reads",
-	".fastq": "reads",
-    	".faa": "feature_protein_fasta"
+    	"fna": "contigs",
+    	"faa": "feature_protein_fasta"
 }
 
 var WorkspaceTypes = [
